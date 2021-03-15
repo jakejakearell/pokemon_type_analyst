8c4a9b4793ddedd24255dd44001ff3f152586414
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-15 15:41:57 -0600

    Add route for displaying team page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :pokemon do
+    resources :team, only: [:show] do
+      resources :dashboard, only: [:index]
+    end
+  end
 end
