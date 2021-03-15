c3246585a9b89ce2d39fbe23f13d7a6735e5f0e0
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-15 15:40:11 -0600

    Add pokemon relationship test

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe Team, type: :model do
   describe 'relationships' do
     it { should have_many :pokemon_teams }
+    it { should have_many(:pokemon).through(:pokemon_teams) }
   end
-  # 
+  #
   # describe 'instance methods' do
   #   describe '#method_name' do
   #     it 'what it does' do
