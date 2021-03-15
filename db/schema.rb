0ef76ab4466fbdb38c51803e553976ffd83d08a6
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-15 15:12:18 -0600

    Change type to primary_type to avoid naming conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_203609) do
+ActiveRecord::Schema.define(version: 2021_03_15_210537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,8 @@ ActiveRecord::Schema.define(version: 2021_03_15_203609) do
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
     t.integer "pokedex"
-    t.string "type"
+    t.string "primary_type"
+    t.string "secondary_type"
   end
 
   create_table "teams", force: :cascade do |t|
